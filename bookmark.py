cdd676a1a2eb50db287da17f8b8de133a42dd3bd
Sana Javed <sanajaved7@gmail.com>
2015-09-02 22:32:56 -0400

    Updated templates and views

--- bookmark.py
+++ bookmark.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+class User:
+    def __init__(self, firstname, lastname):
+        self.firstname = firstname
+        self.lastname = lastname
+
+    def initials(self):
+        return "{}.{}.".format(self.firstname[0], self.lastname[0])
+
+    def __str__(self):
+        return "%s %s" %(self.firstname, self.lastname )
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title="Title passed from view to template", user=User("Sana", "Javed"))
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
